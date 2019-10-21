8b81207cc98e66ac154634ed2f3a1265a196ecee
galaxyswift <galaxyswift@users.noreply.github.com>
2019-10-21 01:35:48 -0500

    upload

---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+/*  Copyright 2014 gokoururi
+    This work is free. You can redistribute it and/or modify it under the
+    terms of the Do What The Fuck You Want To Public License, Version 2,
+    as published by Sam Hocevar. See the COPYING file for more details. */
+
+/*  Remove mascot class from element "main" if its width is <= mascotMinWidth */
+function controlMascot(mascot, mascotMinWidth) {
+    $(window).resize(function(event) {
+        if ( $(window).width() <= mascotMinWidth && $("main").hasClass("mascot") ) {
+            removeMascot();
+        } else if( $(window).width() > mascotMinWidth && ! $("main").hasClass("mascot") ) {
+            setMascot(mascot);
+        }
+    });
+}
+
+function setMascot(mascot) {
+    $('main').addClass("mascot");
+    $('main').css("background-image", "url(" + mascot + ")");
+    $('main').removeClass("plain");
+}
+
+function removeMascot() {
+    $('main').removeClass("mascot");
+    $('main').css("background-image", "");
+    $('main').addClass("plain");
+}
+
+$(document).ready(function(event) {
+    var mascotEnable    = true;
+    var mascotPath      = "images/mascots/"
+    var mascotList      = [ '01.png', '02.png', '03.png', '04.png', '05.png', '06.png', '07.png', '08.png', '09.png', '10.png', '11.png', '12.png', '13.png', '14.png', '15.png', '16.png', '17.png', '18.png', '19.png', '20.png', '21.png', '22.png', '23.png', '24.png', '25.png', '26.png', '27.png', '29.gif', '30.gif', '31.gif', '32.gif', '33.gif', '34.gif', '35.gif', '36.gif', '37.gif', ];
+
+    var mascot          = mascotPath + mascotList[Math.floor(Math.random() * mascotList.length)];
+    var mascotMinWidth  = '750';
+
+    if ( mascotEnable ) { 
+        setMascot(mascot);
+        controlMascot(mascot, mascotMinWidth);
+    } else { removeMascot(); }
+});
